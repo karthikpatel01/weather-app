857ff60dd89d019303b4c9e153b6fb75ceb96a02
karthikpatel01 <karthiksigam143@gmail.com>
2025-09-18 14:45:08 +0530

    Update jspro.js

--- jspro.js
+++ jspro.js
@@ -8,35 +8,37 @@ function getWeather() {
 
 //using async and await fetching the data city wise
 async function showWeather(city) {
-    const url = `https://open-weather13.p.rapidapi.com/city?city=${city}&lang=EN`;
-    const options = {
-        method: 'GET',
-        headers: {
-            'x-rapidapi-key': '1b32c3b37fmsh869396a329e31bbp12c15djsn426915319b0e',
-            'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-        }
-    };
+   const url = `https://open-weather13.p.rapidapi.com/city?city=${city}&lang=EN`;
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '113563c4afmsha27d60b44378d40p13d956jsn21eddc8b9184',
+		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+	}
+};
 
-    try {
-        const response = await fetch(url, options);
-        if(!response){
-            throw new Error("wether not supported")
-        }
-        const result = await response.json();
-        //function calling for append the data to the body
-        getdata(result)
-    } catch (error) {
-        console.error(error);
+
+
+  try {
+    const response = await fetch(url, options);
+    if (!response) {
+      throw new Error("Weather not supported");
     }
-
+    const result = await response.json();
+    getdata(result);
+  } catch (error) {
+    console.log(error);
+    document.getElementById("main").innerHTML = `<p style="color:red;">❌ City not found </p>`;
+  }
 }
-function getdata(result){
+
+
+function getdata(result) {
     //accessing the element for append the data to the body
-    let item=document.getElementById("main")
-    item.innerHTML=
-    `<p>city: ${result.name}</p>
+    let item = document.getElementById("main")
+    item.innerHTML =
+        `<p>city: ${result.name}</p>
     <p>temparature: ${result.main.temp}</p>
     `
-    // document.body.appendChild(item)
-
+    
 }
